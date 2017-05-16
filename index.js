a222b47a6714344413f4414c1bb033c0b23c8522
James Correa <jimmycorrea701@hotmail.com>
2017-05-16 09:30:38 -0400

    waffle

--- index.js
+++ index.js
@@ -10,18 +10,34 @@ const handleSubmit = (ev) => {
   const age = form.age.value
   const birthplace = form.birthplace.value
 
-  const colorDiv = `
-    <div style="height: 50px; width: 100px; background-color: ${hairColor}"></div>
-  `
+  const colorDiv = `<div style="height: 50px; width: 100px; background-color: ${hairColor}"></div>`
 
-  details.innerHTML = `
-    <ul>
-      <li>Name: ${personName}</li>
-      <li>Hair Color: ${colorDiv}</li>
-      <li>Age: ${age}</li>
-      <li>Birthplace: ${birthplace}</li>
-    </ul>
-  `
+  //details.innerHTML = `
+    //<ul>
+      //<li>Name: ${personName}</li>
+      //<li>Hair Color: ${colorDiv}</li>
+      //<li>Age: ${age}</li>
+      //<li>Birthplace: ${birthplace}</li>
+    //</ul>
+
+    const info = document.createElement('ul')
+    details.appendChild(info)
+    const name = document.createElement('li')
+    name.innerHTML = `name: ${personName}`
+    info.appendChild(name)
+    const hair = document.createElement('li')
+    hair.innerHTML = `hair: ${colorDiv}`
+    info.appendChild(hair)
+    const age = document.createElement('li')
+    age.innerHTML = `age: ${age}`
+    info.appendChild(age)
+    const birth = document.createElement('li')
+    birth.innerHTML = `birth: ${birthplace}`
+    info.appendChild(birth)
+
+    
+
+
 }
 
 personForm.addEventListener('submit', handleSubmit)
